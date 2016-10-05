6f21217310c6905762d574e7df54a97dc364f7d0
Chad Lee <chad.lee@archoninfosys.com>
2016-10-05 17:44:05 -0500

    Dumb mistake

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import noPluralFilenames from "./rules/no-plural-filenames";
 import noSingularFilenames from "./rules/no-singular-filenames";
 
 module.exports = {
-	"no-plural-filenames": noPluralFilenames,
-	"no-singular-filenames": noSingularFilenames
+	rules: {
+		"no-plural-filenames": noPluralFilenames,
+		"no-singular-filenames": noSingularFilenames
+	}
 };
